a2e3f024e7bb5c6e7ba3debb58e8d543f646f460
roche-MH <53164586+roche-MH@users.noreply.github.com>
2020-11-11 23:45:12 +0900

    Delete 2.py

--- 11월/2.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    zero,i = 0,0
-    while s != '1':
-        i+=1
-        zero+=s.count('0')
-        s=''.join(s.split('0'))
-        s = str(bin(len(s))[2:])
-    return [i,zero]
-print(solution(	"110010101001"))
